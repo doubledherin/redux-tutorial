956efa6ffe05554452009a211e9491c133e2ef2b
Wendy Dherin <wendydherin@gmail.com>
2016-05-29 13:10:24 -0700

    Add a reducer and tests around it.

--- src/core.js
+++ src/core.js
@@ -1,5 +1,7 @@
 import {List, Map} from 'immutable'
 
+export const INITIAL_STATE = Map()
+
 export function setEntries(state, entries) {
   return state.set('entries', List(entries))
 }
@@ -14,7 +16,7 @@ export function next(state) {
     return state.merge({
       vote: Map({pair: entries.take(2)}),
       entries: entries.skip(2)
-    })    
+    })
   }
 }
 export function vote(state, entry) {
