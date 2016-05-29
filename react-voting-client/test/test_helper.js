4f49da1e80ab823e01a961ba5d71671cff0f65c6
Wendy Dherin <wendydherin@gmail.com>
2016-05-29 15:56:28 -0700

    Install mocha and chai and add test commands to scripts in package.json file.

--- react-voting-client/test/test_helper.js
+++ react-voting-client/test/test_helper.js
@@ -1,4 +1,6 @@
-import jsdom from 'jsdom'
+import jsdom from 'jsdom';
+import chai from 'chai';
+import chaiImmutable from 'chai-immutable';
 
 const doc = jsdom.jsdom('<!doctype html><html><body></body></html>')
 const win = jsdom.defaultView
@@ -11,3 +13,4 @@ Object.keys(window).forEach((key) => {
     global[key] = window[key]
   }
 })
+chai.use(chaiImmutable)
