6abb4cc020014745fc14086a28505db60c912d12
Wendy Dherin <wendydherin@gmail.com>
2016-05-30 12:08:36 -0700

    Reset entries when vote has occured. Write unit test around this.

--- react-voting-client/test/reducer_spec.js
+++ react-voting-client/test/reducer_spec.js
@@ -94,4 +94,27 @@ describe('reducer', () => {
       }
     }))
   })
+  it('removes hasVoted on SET_STATE if the entry pair changes', () => {
+    const initialState = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      },
+      hasVoted: 'Trainspotting'
+    })
+    const action = {
+      type: 'SET_STATE',
+      state: {
+        vote: {
+          pair: ['Sunshine', 'Slumdog Millionaire']
+        }
+      }
+    }
+    const nextState = reducer(initialState, action)
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Sunshine', 'Slumdog Millionaire']
+      }
+    }))
+  })
 })
