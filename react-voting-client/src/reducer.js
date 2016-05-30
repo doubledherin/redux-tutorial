b1cb6786f48f5828dd7dd45d786f664ff0779a85
Wendy Dherin <wendydherin@gmail.com>
2016-05-30 11:53:56 -0700

    Handle Vote action and case where vote is on an invalid entry; write unit tests around it.

--- react-voting-client/src/reducer.js
+++ react-voting-client/src/reducer.js
@@ -3,6 +3,16 @@ import {Map} from 'immutable'
 function setState(state, newState) {
   return state.merge(newState)
 }
+
+function vote(state, entry) {
+  const currentPair = state.getIn(['vote', 'pair'])
+  if (currentPair && currentPair.includes(entry)) {
+    return state.set('hasVoted', entry)
+  } else {
+    return state
+  }
+}
+
 export default function(state = Map(), action) {
   switch(action.type) {
     case 'SET_STATE':
--- react-voting-client/test/reducer_spec.js
+++ react-voting-client/test/reducer_spec.js
@@ -61,4 +61,37 @@ describe('reducer', () => {
       }
     }))
   })
+  it('handles VOTE action by setting hasVoted', () {
+    const state = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later']
+        tally: {Trainspotting: 1}
+      }
+    })
+    const action = {type: 'VOTE', entry: 'Trainspotting'}
+    const nextState = reducer(state, action)
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      },
+      hasVoted: 'Trainspotting'
+    }))
+  })
+  it('does not set hasVoted for VOTE action on an invalid entry', () => {
+    const state = fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    })
+    const action = {type: 'VOTE', entry: 'Sunshine'}
+    const nextState = reducer(state, action)
+    expect(nextState).to.equal(fromJS({
+      vote: {
+        pair: ['Trainspotting', '28 Days Later'],
+        tally: {Trainspotting: 1}
+      }
+    }))
+  })
 })
